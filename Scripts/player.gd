5ddd0f9fd1593da0e0b7879aaac405de4f3b2964
MagmaC4 <magmac495@gmail.com>
2026-04-05 01:11:26 -0500

    Add slide animations

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -5,6 +5,7 @@ var SPEED = 5.0
 var JUMP_VELOCITY = 4.5
 var dev_mode_enabled : bool = false
 var DEV_MODE_SPEED := 25.0
+var is_sliding : bool
 var slide_accel = 5000
 
 # Auxillary
@@ -19,8 +20,7 @@ func _physics_process(delta: float) -> void:
 	else:
 		handle_movement(delta)
 	
-	if (Input.is_action_pressed("slide")):
-		handle_slide(delta)
+	handle_slide(delta)
 
 func _process(delta):
 	toggle_dev_mode()
@@ -39,17 +39,33 @@ func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 		anim_player.play("weapon_idle")
 		weapon_hitbox.monitorable = false
 
+
 # MOVEMENT ====================================================================
 func handle_slide(delta):
 	var floor_normal : Vector3 = get_floor_normal()
 	var gravity : Vector3 = Vector3.DOWN
 	var slope_dir : Vector3 = (gravity - floor_normal * gravity.dot(floor_normal)).normalized()
-
 	var slope_angle = rad_to_deg(acos(floor_normal.dot(Vector3.UP)))
-	if is_on_floor() and slope_angle > 5:
+	
+	# ONLY APPLY SLIDE WHEN PLAYER IS ON GROUND AND ON STEEP SLOPE
+	if Input.is_action_pressed("slide") and is_on_floor() and slope_angle > 10: 
+		is_sliding = true
 		var diff = slope_dir * slide_accel * delta
-		print("slide velocity delta: " + str(diff))
-		velocity += diff
+		diff.z *= 2 # double gravity 
+		velocity += diff # apply slope slide to player velocity
+		
+		if (Input.is_action_just_pressed("slide")):
+			anim_player.play("cam_slide_start")
+		
+	elif Input.is_action_just_released("slide"):
+		anim_player.play("cam_slide_end")
+		
+	else:
+		is_sliding = false
+
+		
+	
+
 
 func handle_movement_dev_mode(delta):
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
