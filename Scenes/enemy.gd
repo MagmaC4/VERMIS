6eeccf471fe00f708a81dc445f533a577f407c3a
MagmaC4 <magmac495@gmail.com>
2026-04-04 22:25:27 -0500

    Add enemy health

--- Scenes/enemy.gd
+++ Scenes/enemy.gd
@@ -1,16 +1,21 @@
 extends Node3D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@export var max_health := 100
+@onready var health := max_health
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func take_damage(amount : int):
+	health -= amount
+	if (health < 0):
+		health = 0
+
+func heal(amount : int):
+	health += amount
+	if (health > max_health):
+		health = max_health
 	
 	
 func _on_hitbox_area_entered(area: Area3D) -> void:
 	if (area.is_in_group("Weapon")):
-		print("doubie")
+		take_damage(5)
+		print("Health: " + str(health))
