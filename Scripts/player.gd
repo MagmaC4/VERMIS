2fffd8b4a47e076fe0d6e1ef8dba7664984b940d
MagmaC4 <magmac495@gmail.com>
2026-04-05 00:24:49 -0500

    Add slide mechanic

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,10 +1,11 @@
 extends CharacterBody3D
 
 # Movement
+var SPEED = 5.0
+var JUMP_VELOCITY = 4.5
 var dev_mode_enabled : bool = false
 var DEV_MODE_SPEED := 25.0
-var SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+var slide_accel = 5000
 
 # Auxillary
 @onready var camera : Camera3D = $Camera3D
@@ -17,6 +18,9 @@ func _physics_process(delta: float) -> void:
 		handle_movement_dev_mode(delta)
 	else:
 		handle_movement(delta)
+	
+	if (Input.is_action_pressed("slide")):
+		handle_slide(delta)
 
 func _process(delta):
 	toggle_dev_mode()
@@ -36,6 +40,17 @@ func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 		weapon_hitbox.monitorable = false
 
 # MOVEMENT ====================================================================
+func handle_slide(delta):
+	var floor_normal : Vector3 = get_floor_normal()
+	var gravity : Vector3 = Vector3.DOWN
+	var slope_dir : Vector3 = (gravity - floor_normal * gravity.dot(floor_normal)).normalized()
+
+	var slope_angle = rad_to_deg(acos(floor_normal.dot(Vector3.UP)))
+	if is_on_floor() and slope_angle > 5:
+		var diff = slope_dir * slide_accel * delta
+		print("slide velocity delta: " + str(diff))
+		velocity += diff
+
 func handle_movement_dev_mode(delta):
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	var direction := camera.global_basis * Vector3(input_dir.x, 0, input_dir.y).normalized()
@@ -47,7 +62,7 @@ func handle_movement(delta):
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("move_jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
