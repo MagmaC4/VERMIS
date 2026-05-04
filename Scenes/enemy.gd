438b8b0ca25d53cb194e71ce2833c2d90946e437
MagmaC4 <magmac495@gmail.com>
2026-05-03 19:51:46 -0500

    Add explosion on enemy death

--- Scenes/enemy.gd
+++ Scenes/enemy.gd
@@ -1,18 +1,32 @@
 extends Node3D
 
-@export var max_health := 100
+@export var max_health := 30
 @onready var health := max_health
 
 
 func take_damage(amount : int):
 	health -= amount
-	if (health < 0):
+	if (health <= 0):
 		health = 0
+		on_death()
 
 func heal(amount : int):
 	health += amount
 	if (health > max_health):
 		health = max_health
+		
+func on_death():
+	var explosion_anim_1 = $AnimatedSprite3D
+	var explosion_sfx = $AudioStreamPlayer3D
+	
+	print("Enemy Died")
+	$MeshInstance3D.hide()
+	$Hitbox.set_deferred("monitoring", false)
+	explosion_anim_1.play()
+	explosion_sfx.play()
+	get_tree().create_timer(1.0).timeout.connect(queue_free)
+
+	
 	
 	
 func _on_hitbox_area_entered(area: Area3D) -> void:
